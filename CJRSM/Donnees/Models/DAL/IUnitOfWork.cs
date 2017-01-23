bd8e6f8b1dd3d43290b2b99d51b3d7b185476c26
Guillaume Barbier <lecompte@hotmail.ca>
2017-01-22 21:44:04 -0500

    Avancement du projet et mise a jour du CSS. Ajout de la base de donnée préliminaire et ajout de quelques vue de liste.

---
+++ CJRSM/Donnees/Models/DAL/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CJRSM.Models.DAL
+{
+    public class IUnitOfWork //: IDisposable
+    {
+       // IGenericRepository<Membre> Membres { get; }
+    }
+}
