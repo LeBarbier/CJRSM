bb07a33388ddc214c6067c6bdf554ca909e7fc00
Guillaume Barbier <Guillaume Barbier>
2017-02-03 12:08:10 -0500

    Création/Ajout des classes de model

---
+++ CJRSM/Donnees/Models/Biz/Activite.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Donnees.Models.Biz
+{
+    public class Activite
+    {
+        public int Id;
+        private string MembreId;
+        public string Titre;
+        public string Jeu;
+        public string Description;
+        public int NbrMembreMin;
+        public int NbrMembreMax;
+        public int Jour;
+        public DateTime HeureDebut;
+        public bool Hebdomadaire;
+        public DateTime DateDebut;
+        public int NbrRepetition;
+        public bool Accepte;
+        private ArrayList Participant;
+
+        /****/
+        Activite(string MembreId, string titre, string description, int jour, DateTime heureDebut, DateTime dateDebut)
+        {
+            this.MembreId = MembreId;
+            this.Titre = titre;
+            this.Description = description;
+            this.Jour = jour;
+            this.HeureDebut = heureDebut;
+            this.DateDebut = dateDebut;
+            this.Hebdomadaire = false;
+            this.Accepte = false;
+            Participant = new ArrayList();
+        }
+
+        Activite(string membreId, string titre, string jeu, string description,
+                int nbrMembreMin, int nbrMembreMax, int jour, DateTime heureDebut,
+                bool hebdomadaire, DateTime dateDebut, int nbrRepetition)
+        {
+            this.MembreId = membreId;
+            this.Titre = titre;
+            this.Jeu = jeu;
+            this.Description = description;
+            this.NbrMembreMin = nbrMembreMin;
+            this.NbrMembreMax = nbrMembreMax;
+            this.Jour = jour;
+            this.HeureDebut = heureDebut;
+            this.Hebdomadaire = hebdomadaire;
+            this.DateDebut = dateDebut;
+            this.NbrRepetition = nbrRepetition;
+            this.Accepte = false;
+            Participant = new ArrayList();
+    }
+
+        public string getMembreId()
+        {
+            return MembreId;
+        }
+        public void setMembreId(string MembreId)
+        {
+            this.MembreId = MembreId;
+        }
+        public ArrayList getParticipant()
+        {
+            return Participant;
+        }
+        public void AjouterParticipant(string MembreId)
+        {
+            Participant.Add(MembreId);
+        }
+        public void AccepteActivite()
+        {
+            this.Accepte = true;
+        }
+        /*public ArrayList getActivite()
+        {
+            return this;
+        }*/
+    }
+}
---
+++ CJRSM/Donnees/Models/Biz/Publication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Donnees.Models.Biz
+{
+    public class Publication
+    {
+        public int Id;
+        public string Titre;
+        private string MembreId;
+        public string contenu;
+
+        public string getMembreId()
+        {
+            return MembreId;
+        }
+        public void setMembreId(string MembreId)
+        {
+            this.MembreId = MembreId;
+        }
+    }
+}
