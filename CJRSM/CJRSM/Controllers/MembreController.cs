3a31ed0916e34ad0d337612c41269a1607de02cc
Guillaume Barbier <lecompte@hotmail.ca>
2017-02-12 22:06:21 -0500

    Good

--- CJRSM/CJRSM/Controllers/MembreController.cs
+++ CJRSM/CJRSM/Controllers/MembreController.cs
@@ -1,12 +1,8 @@
-﻿using CJRSM.Models.Biz;
-using CJRSM.Models.DAL;
+﻿using CJRSM.Models.DAL;
 using CJRSM.Models.View.Membre;
 using Scrypt;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 
