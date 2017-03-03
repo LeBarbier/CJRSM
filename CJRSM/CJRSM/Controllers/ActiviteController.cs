21ee1bb950ff9f6d5dca92778dda7fb10b468c58
Guillaume Barbier <lecompte@hotmail.ca>
2017-03-03 00:39:35 -0500

    Avancement de la recherche d'activite

--- CJRSM/CJRSM/Controllers/ActiviteController.cs
+++ CJRSM/CJRSM/Controllers/ActiviteController.cs
@@ -26,12 +26,135 @@ namespace CJRSM.Controllers
             repo = contexte.Activite;
         }
 
-        public ActionResult Index(string chercherTitre)
+        public ActionResult Index(string chercherTitre = "")
         {
             if (chercherTitre == null)
                 chercherTitre = "";
             IEnumerable<Activite> listeActivite = repo.Get(a => a.Titre.Contains(chercherTitre)).Select(a => a);
             return View(listeActivite);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DateTime? heure = null,
+                                int id = 0,
+                                string chercherTitre = "",
+                                int nbrPlaceRestante = 0,
+                                int jour = 0,
+                                int recursif = 0)
+        {
+            // Création du dictionnaire
+            Dictionary<int, Activite> dictionnaireActivite = new Dictionary<int, Activite>();
+            IEnumerable<Activite> iEnumActivite = repo.Get(j => j.Titre.Contains(""));
+            List<Activite> listeActivite = new List<Activite>();
+            listeActivite = iEnumActivite.ToList();
+            IEnumerable<Participant> iEnumParticipant = contexte.Participant.Get(j => j.Id.ToString().Contains(""));
+            List<Participant> listeParticipant = iEnumParticipant.ToList();
+
+            // Attribution de toutes les valeurs ainsi que des clefs au dicitonnaire
+            for (int i = 0; i < iEnumActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+
+            // Trie du dictionnaire selon l'utilisateur
+            if (chercherTitre != "" && chercherTitre != null)
+            {
+                for (int i = 0; i <= dictionnaireActivite.Count(); i++)
+                {
+                    if (!dictionnaireActivite[i].Titre.Contains(chercherTitre))
+                    {
+                        dictionnaireActivite.Remove(i);
+                    }
+                }
+            }
+            listeActivite = dictionnaireActivite.Values.ToList();
+            dictionnaireActivite = new Dictionary<int, Activite>();
+            for (int i = 0; i < listeActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+            if (id != 0)
+            {
+                dictionnaireActivite = new Dictionary<int, Activite>();
+                activite = contexte.Activite.Find(id);
+                dictionnaireActivite.Add(1, activite);
+            }
+
+            listeActivite = dictionnaireActivite.Values.ToList();
+            dictionnaireActivite = new Dictionary<int, Activite>();
+            for (int i = 0; i < listeActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+            if (nbrPlaceRestante != 0)
+            {
+                for (int i = 0; i <= dictionnaireActivite.Count(); i++)
+                {
+                    int nbrPlaceRestanteCount = listeParticipant.Count();
+                    for (int j = 0; j < listeParticipant.Count(); j++)
+                    {
+                        if (listeParticipant[j].IdActivite == activite.Id)
+                        {
+                            nbrPlaceRestante--;
+                        }
+                    }
+                    if (nbrPlaceRestanteCount <= nbrPlaceRestante)
+                    {
+                        dictionnaireActivite.Remove(i);
+                    }
+                }
+            }
+            listeActivite = dictionnaireActivite.Values.ToList();
+            dictionnaireActivite = new Dictionary<int, Activite>();
+            for (int i = 0; i < listeActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+            if (jour != 0)
+            {
+                for (int i = 0; i <= dictionnaireActivite.Count(); i++)
+                {
+                    if (dictionnaireActivite[i].Jour != jour)
+                    {
+                        dictionnaireActivite.Remove(i);
+                    }
+                }
+            }
+            listeActivite = dictionnaireActivite.Values.ToList();
+            dictionnaireActivite = new Dictionary<int, Activite>();
+            for (int i = 0; i < listeActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+            if (recursif != 0)
+            {
+                for (int i = 0; i <= dictionnaireActivite.Count(); i++)
+                {
+                    if (dictionnaireActivite[i].NbrRepetion != recursif)
+                    {
+                        dictionnaireActivite.Remove(i);
+                    }
+                }
+            }
+            listeActivite = dictionnaireActivite.Values.ToList();
+            dictionnaireActivite = new Dictionary<int, Activite>();
+            for (int i = 0; i < listeActivite.Count(); i++)
+            {
+                dictionnaireActivite.Add(i, listeActivite[i]);
+            }
+            if (heure != null)
+            {
+                for (int i = 0; i <= dictionnaireActivite.Count(); i++)
+                {
+                    if (!(dictionnaireActivite[i].HeureDebut.Equals(heure)))
+                    {
+                        dictionnaireActivite.Remove(i);
+                    }
+                }
+            }
+            // Retour du dictionnaire a un IEnumerable pour le retour a la page HTML
+            iEnumActivite = dictionnaireActivite.Values;
+            return View(iEnumActivite);
         }
 
         public ActionResult Details(int id)
